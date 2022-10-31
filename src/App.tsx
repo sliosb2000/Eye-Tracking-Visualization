21b0a552271bb0a6ffa0b98f0824feb5b439c79b
Bryan Tran <bryantran78@gmail.com>
2022-10-31 10:54:52 -0700

    Revert "cleanup"
    This reverts commit d3022e1fb50da6d2bfcc6d238a1cf4a86246f4fb.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import BubbleChartCard from './components/BubbleChartCard';
 
 class App extends React.Component {
   render() {
+    
+  
     return (
       <div className="App">
         <BubbleChartCard
