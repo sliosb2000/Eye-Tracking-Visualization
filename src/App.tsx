8706966a9a7d0a4b5a93f7fd88706d7d9f47c729
Bryan Tran <bryantran78@gmail.com>
2022-10-30 17:47:39 -0700

    dummy values

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ export const data = {
     {
       label: 'Red dataset',
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
+        x: faker.datatype.number({ min: 0, max: 1600 }),
+        y: faker.datatype.number({ min: 0, max: 1200 }),
         r: faker.datatype.number({ min: 5, max: 20 }),
       })),
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
@@ -33,8 +33,8 @@ export const data = {
     {
       label: 'Blue dataset',
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
+        x: faker.datatype.number({ min: 0, max: 1600 }),
+        y: faker.datatype.number({ min: 0, max: 1200 }),
         r: faker.datatype.number({ min: 5, max: 20 }),
       })),
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
