bbfac9708cc35eceab4c4a8454e8a54aad000ab3
Bryan Tran <bryantran78@gmail.com>
2022-10-30 17:34:14 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bubble } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
+
+export const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+export const data = {
+  datasets: [
+    {
+      label: 'Red dataset',
+      data: Array.from({ length: 50 }, () => ({
+        x: faker.datatype.number({ min: -100, max: 100 }),
+        y: faker.datatype.number({ min: -100, max: 100 }),
+        r: faker.datatype.number({ min: 5, max: 20 }),
+      })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Blue dataset',
+      data: Array.from({ length: 50 }, () => ({
+        x: faker.datatype.number({ min: -100, max: 100 }),
+        y: faker.datatype.number({ min: -100, max: 100 }),
+        r: faker.datatype.number({ min: 5, max: 20 }),
+      })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Bubble Chart</h1>
+        <Bubble options={options} data={data} />
+      </div>
+    );
+  }
+}
+
+export default App;
