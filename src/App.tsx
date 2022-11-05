02f01902d58c95dc228e60c9a69e658237c505b9
Bryan Tran <bryantran78@gmail.com>
2022-11-04 22:10:02 -0700

    fix cached

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Divider, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import React from 'react';
 import BubbleChartCard from './components/ChartCard';
-import { DataFiles, participants, VisualizationType } from './data/dataFIX';
+import { DataFiles, participants, VisualizationType } from './data/data';
 import { ontologyMap } from './data/types/additional/additional';
 import './App.css';
 
